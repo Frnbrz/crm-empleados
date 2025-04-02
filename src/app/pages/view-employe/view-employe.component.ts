d84cd47e4daa9fe1facb6f4e423bf660f680e0d0
frnbrz <frnbrz@gmail.com>
2025-04-02 12:25:22 +0200

    Implement employee detail view with data binding and service integration

--- src/app/pages/view-employe/view-employe.component.ts
+++ src/app/pages/view-employe/view-employe.component.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
+import { EmployeesService } from '../../services/employees.service';
+import { Employee } from '../../interfaces/employee.interface';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-view-employe',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './view-employe.component.html',
   styleUrl: './view-employe.component.css',
 })
-export class ViewEmployeComponent {}
+export class ViewEmployeComponent {
+  @Input() idEmployee: string = '';
+  employeeService = inject(EmployeesService);
+  employee: Employee | undefined;
+
+  async ngOnInit() {
+    this.employee = await this.employeeService.getById(this.idEmployee);
+  }
+}
